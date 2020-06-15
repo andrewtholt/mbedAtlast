828d52155abd64f10b163eedba83d70ae7c310ca
Andrew Holt <andrewtholt60@gmail.com>
2020-06-15 13:43:14 +0100

    Removed redundunat code from atlast.c and added Small.cpp and Small.h

---
+++ Small.cpp
@@ -0,0 +1,106 @@
+/***********************************************************************
+ * AUTHOR: andrewh <andrewh>
+ *   FILE: ./Small.cpp
+ *   DATE: Sat Jun 13 13:36:06 2020
+ *  DESCR: 
+ ***********************************************************************/
+#include "Small.h"
+#include <iostream>
+
+/***********************************************************************
+ *  Method: Small::Small
+ *  Params: 
+ * Effects: 
+ ***********************************************************************/
+Small::Small() {
+}
+
+
+/***********************************************************************
+ *  Method: Small::~Small
+ *  Params: 
+ * Effects: 
+ ***********************************************************************/
+Small::~Small() {
+}
+
+
+/***********************************************************************
+ *  Method: Small::Get
+ *  Params: std::string key
+ * Returns: std::string
+ * Effects: 
+ ***********************************************************************/
+std::string Small::Get(std::string key) {
+
+    std::string res="NON";
+
+    int cnt = db.count(key);
+    if (cnt > 0) {
+        res = db[key]->value;
+    } else {
+        res="NON";
+    }
+
+    return res;
+}
+
+/***********************************************************************
+ *  Method: Small::Set
+ *  Params: std::string key, std::string value
+ * Returns: void
+ * Effects: 
+ ***********************************************************************/
+void Small::Set(std::string key, std::string value) {
+    int cnt = db.count(key);
+
+    // TODO Smart pointer ?
+    if( cnt > 0) {
+        datum *p = db[key];
+        p->value = value;
+
+        for(auto sub : db[key]->subscriber ) {
+            sendSet(sub, key, value );
+        }
+    } else {
+        db[key] = new datum(value);
+    }
+}
+
+
+/***********************************************************************
+ *  Method: Small::sendSet
+ *  Params: std::string key
+ * Returns: void
+ * Effects: 
+ ***********************************************************************/
+void Small::sendSet(uint8_t id,std::string key, std::string value) {
+//    std::cout << "========> Send set to " << to_string(id) << " Key "<< key << "=" << value << "\n";
+}
+
+void Small::Sub(std::string key, uint8_t id) {
+
+    int cnt = db.count(key);
+    if (cnt > 0) {
+        (db[key]->subscriber).insert(id);
+
+        sendSet(id, key, db[key]->value);
+    } 
+
+}
+
+void Small::dump() {
+    std::cout << "======= Dump ======" << "\n";
+    for(const auto &myPair : db ) {
+        std::string key = myPair.first;
+        std::string v   = db[key]->value;
+        std::cout << "Key:" << key << " Value:" << v << "\n";
+        std::cout << "Sub";
+        for(auto sub : db[key]->subscriber ) {
+            std::cout << "\t" << sub << "\n";
+        }
+    }
+}
+
+
+
---
+++ Small.h
@@ -0,0 +1,30 @@
+#include <string>
+#include <map>
+#include <set>
+
+class datum {
+    public:
+    std::string value;
+    std::set<int>subscriber;
+
+    datum(std::string v) {
+        value=v;
+        subscriber={};
+    }
+} ;
+
+class Small {
+    public:
+        Small();
+        ~Small();
+
+        std::string Get(std::string key);
+        void Set(std::string key, std::string value);
+        void Sub(std::string key, uint8_t id);
+        void dump();
+
+        virtual void sendSet(uint8_t id,std::string key, std::string value);
+    private:
+        std::map<std::string, datum *> db;
+};
+
